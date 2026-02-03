465ffbe3a27b41a5f2e8d3b638653aa76462cd8b
why <Haoyang.Wu@marsladder.com.au>
2026-02-03 18:34:21 +0800

    chore: reorder virtual environment activation in run_paddle_ocr.sh

--- run_paddle_ocr.sh
+++ run_paddle_ocr.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 # PaddleOCR 便捷运行脚本
-
-# 激活虚拟环境
-source venv/bin/activate
-
 # 运行 PaddleOCR
 # 用法: ./run_paddle_ocr.sh <输入目录或文件> <输出.md> [语言]
 # 示例: ./run_paddle_ocr.sh test_png output.md ch
+
+source venv/bin/activate
 python paddle_ocr_to_md.py "$@"
